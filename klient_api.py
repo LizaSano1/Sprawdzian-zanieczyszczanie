d701e3362d91282802fac801be8eeed752a6ddff
LizaSano1 <145109365+LizaSano1@users.noreply.github.com>
2024-04-29 11:34:56 +0200

    noweee

--- klient_api.py
+++ klient_api.py
@@ -1,13 +1,14 @@
 import requests
 
-class AirQualityClient:
-    def __init__(self, token):
-        self.token = token
-        self.base_url = "https://api.airvisual.com/v2"
+class AirQualityAPIClient:
+    def __init__(self, api_token):
+        self.api_token = api_token
+        self.base_url = "https://api-docs.iqair.com"
 
     def get_air_quality_data(self, city):
-        endpoint = f"{self.base_url}/city?city={city}&state=Warsaw&country=Poland&key={self.token}"
-        response = requests.get(endpoint)
+        headers = {"Authorization": f"Bearer {self.api_token}"}
+        params = {"city": city}
+        response = requests.get(f"{self.base_url}/data", headers=headers, params=params)
         if response.status_code == 200:
             return response.json()
         else:
