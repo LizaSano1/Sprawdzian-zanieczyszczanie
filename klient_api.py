036ca8857472f8dd4eebdd20bf7272f8bc5b05d4
LizaSano1 <145109365+LizaSano1@users.noreply.github.com>
2024-04-29 11:33:53 +0200

    sgro

--- klient_api.py
+++ klient_api.py
@@ -1,15 +1,14 @@
 import requests
 
-class KlientAPI:
-    def __init__(self, token_api):
-        self.token_api = token_api
+class AirQualityClient:
+    def __init__(self, token):
+        self.token = token
+        self.base_url = "https://api.airvisual.com/v2"
 
-    def pobierz_dane_zanieczyszczenia_powietrza(self, miasto):
-        url = f"https://api-docs.iqair.com/api/v1/{miasto}/"
-        headers = {"Authorization": f"Bearer {self.token_api}"}
-        response = requests.get(url, headers=headers)
+    def get_air_quality_data(self, city):
+        endpoint = f"{self.base_url}/city?city={city}&state=Warsaw&country=Poland&key={self.token}"
+        response = requests.get(endpoint)
         if response.status_code == 200:
             return response.json()
         else:
-            print("Błąd pobierania danych:", response.status_code)
             return None
